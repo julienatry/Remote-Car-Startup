ce43ca985af94d756c016f9e9fd6c8e5ca9dda0c
Julien Atry <atryjulien@gmail.com>
2020-11-02 23:55:59 +0100

    Update IC_Program.ino
    Working Bluetooth reading

--- IC_Program.ino
+++ IC_Program.ino
@@ -3,7 +3,7 @@
 int warningLightsPin = 8;
 int ignitionPin = 9;
 int starterPin = 10;
-char receivedData;
+String receivedData;
 
 SoftwareSerial BTSerial(2, 3); // RX, TX pins for BT module
 
@@ -11,6 +11,7 @@ void setup() {
   pinMode(warningLightsPin, OUTPUT);
   pinMode(ignitionPin, OUTPUT);
   pinMode(starterPin, OUTPUT);
+  pinMode(13, OUTPUT);
 
   Serial.begin(9600);
   BTSerial.begin(9600);
@@ -19,7 +20,12 @@ void setup() {
 void loop() {
 	if (BTSerial.available())
 	{
-		receivedData = BTSerial.read();
-		Serial.write(receivedData);
+		receivedData = BTSerial.readString();
+	}
+
+	if (receivedData == "StartupSequence")
+	{
+		Serial.print("Starting vehicle");
+		receivedData = ""; // Reset
 	}
 }
