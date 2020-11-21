b89c08c12def91330577e56b28cb0246d0ab18f4
Julien Atry <atryjulien@gmail.com>
2020-11-21 03:03:50 +0100

    Update IC_Program.ino

--- IC_Program.ino
+++ IC_Program.ino
@@ -1,5 +1,7 @@
 #include <SoftwareSerial.h>
 
+int hornUGPin = 7;
+int hornResetPin = 6;
 int warningLightsPin = 8;
 int ignitionPin = 9;
 int starterPin = 10;
@@ -9,7 +11,10 @@ int areAngelEyesON = 0;
 String receivedData;
 char* state = "500";
 
-SoftwareSerial BTSerial(2, 3); 											// RX, TX pins for BT module
+SoftwareSerial BTSerial(2, 3); 											//RX, TX pins for BT module
+SoftwareSerial HornSerial(4, 5);										//RX, TX pins for custom horn
+
+Adafruit_Soundboard horn(&HornSerial, NULL, hornResetPin);
 
 void setup() {
   pinMode(warningLightsPin, OUTPUT);
@@ -17,13 +22,14 @@ void setup() {
   pinMode(starterPin, OUTPUT);
   pinMode(angelEyesPin, OUTPUT);
   pinMode(hornPin, OUTPUT);
+  pinMode(hornUGPin, OUTPUT);
 
   Serial.begin(9600);
   BTSerial.begin(9600);
 }
 
 void loop() {
-	receivedData = ""; 													// Reset
+	receivedData = ""; 													//Reset
 
 	if (BTSerial.available())
 	{
@@ -45,6 +51,9 @@ void loop() {
 	}else if (receivedData == "AreAngelEyesON")
 	{
 		BTSerial.write(angelEyes("state"));								//Send 501 if AngelEyes ON or 500 if OFF
+	}else if (receivedData > 800 && receivedData < 900)
+	{
+		/* code */
 	}
 }
 
@@ -76,4 +85,24 @@ void warningLights() {
 	digitalWrite(warningLightsPin, HIGH);
 	delay(2000);
 	digitalWrite(warningLightsPin, LOW);
+}
+
+
+void initHorn() {
+	digitalWrite(hornUGPin, HIGH);
+	delay(2000);
+	HornSerial.begin(9600);
+
+	if (!sfx.reset())
+	{
+		Serial.println("Horn reset failed");
+	}
+}
+
+void playHorn(int track) {
+	Serial.print("Playing track "); Serial.println(track);
+	if (!sfx.playTrack(track))
+	{
+		Serial.println("Failed to play track");
+	}
 }
